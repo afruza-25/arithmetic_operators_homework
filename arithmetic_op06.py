ad456fdc451e6f8c470646d30173ea84a2c79e21
afruza-25 <118989989+afruza-25@users.noreply.github.com>
2022-11-30 13:54:19 +0500

    changed

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,9 @@
 #Create a variable called 'number' with data type int.
 number=9
 #Divide the number by 2 and assign number.
-a1=(number%2)
+number/=2
 #Find the fraction of the number and assing to a variable called 'answer'.
-answer=a1
+answer=number-number//1
 #Print the answer.
 print(answer)
 #Print the number.
